0d0f7a54751555dd1560287e91b9decc16542d9f
Faust <86347407+F3ust@users.noreply.github.com>
2023-09-23 07:33:19 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-    $dbserver = 'localhost';
-    $dbusername = 'Faust';
-    $dbpassword = 'Phu0ng95crA';
-    $dbname = 'faust1';
+    $dbserver = 'sql207.byethost24.com';
+    $dbusername = 'b24_35089670';
+    $dbpassword = 'Phu0ng@nh';
+    $dbname = 'b24_35089670_faust1';
 
     $connect = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
     if ($connect->connect_error) {
